6ace9d3179966f5b0b265202325c0aa1cf2c9b81
Nirbhik The Nice <nirbhikkumawat@gmail.com>
2025-11-22 21:33:51 +0530

    Added logic to place mines randomly

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 type Cell struct {
 	IsRevealed bool
@@ -17,12 +20,23 @@ func (g *Game) printBoard(r, c int) {
 	for i := 0; i < r; i++ {
 		for j := 0; j < c; j++ {
 			if g.Board[i][j].IsMine {
-				fmt.Print("X")
+				fmt.Print("X ")
 			} else {
 				fmt.Print(". ")
 			}
 		}
 		fmt.Println()
+	}
+}
+func (g *Game) placeMines(mines int) {
+	minesPlaced := 0
+	for minesPlaced < mines {
+		r := rand.Intn(10)
+		c := rand.Intn(10)
+		if !g.Board[r][c].IsMine {
+			g.Board[r][c].IsMine = true
+			minesPlaced++
+		}
 	}
 }
 func NewGame() *Game {
@@ -35,10 +49,10 @@ func NewGame() *Game {
 			g.Board[i][j] = Cell{
 				IsRevealed: false,
 				IsFlagged:  false,
-				IsMine:     false,
 			}
 		}
 	}
+	g.placeMines(10)
 	return g
 }
 func main() {
