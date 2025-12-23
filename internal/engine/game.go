bc8b8b3ae2fdaa4e270080ab5ffa753cbb46b999
Nirbhik The Nice <nirbhikkumawat@gmail.com>
2025-12-23 17:59:51 +0530

    seperated game logic from main.go

---
+++ internal/engine/game.go
@@ -0,0 +1,220 @@
+package engine
+
+import (
+	"math/rand"
+	"os"
+)
+
+type Cell struct {
+	IsRevealed  bool
+	IsFlagged   bool
+	IsMine      bool
+	NearbyMines int
+}
+type Game struct {
+	Rows          int
+	Cols          int
+	GameOver      bool
+	GameWon       bool
+	Board         [][]Cell
+	TotalCells    int
+	RevealedCells int
+	Mines         int
+}
+
+func (g *Game) PlaceMines() {
+	mines := g.Mines
+	minesPlaced := 0
+	for minesPlaced < mines {
+		i := rand.Intn(g.Rows)
+		j := rand.Intn(g.Cols)
+		if !g.Board[i][j].IsMine {
+			g.Board[i][j].IsMine = true
+			minesPlaced++
+		}
+	}
+}
+func (cell *Cell) CheckMine() {
+	if cell.IsMine {
+	} else {
+		cell.NearbyMines++
+	}
+}
+func (g *Game) CountMines() {
+	r := g.Rows
+	c := g.Cols
+	for i := 0; i < r; i++ {
+		for j := 0; j < c; j++ {
+			if g.Board[i][j].IsMine {
+				if i > 0 && i < r-1 && j > 0 && j < c-1 {
+					g.Board[i-1][j-1].CheckMine()
+					g.Board[i-1][j].CheckMine()
+					g.Board[i-1][j+1].CheckMine()
+					g.Board[i][j-1].CheckMine()
+					g.Board[i][j+1].CheckMine()
+					g.Board[i+1][j-1].CheckMine()
+					g.Board[i+1][j].CheckMine()
+					g.Board[i+1][j+1].CheckMine()
+				} else if i == 0 && j == 0 {
+					g.Board[0][1].CheckMine()
+					g.Board[1][0].CheckMine()
+					g.Board[1][1].CheckMine()
+				} else if i == 0 && j == c-1 {
+					g.Board[i][j-1].CheckMine()
+					g.Board[i+1][j].CheckMine()
+					g.Board[i+1][j-1].CheckMine()
+				} else if i == r-1 && j == 0 {
+					g.Board[i-1][j].CheckMine()
+					g.Board[i][j+1].CheckMine()
+					g.Board[i-1][j+1].CheckMine()
+				} else if i == r-1 && j == c-1 {
+					g.Board[i-1][j-1].CheckMine()
+					g.Board[i-1][j].CheckMine()
+					g.Board[i][j-1].CheckMine()
+				} else if i == 0 {
+					g.Board[i][j-1].CheckMine()
+					g.Board[i][j+1].CheckMine()
+					g.Board[i+1][j].CheckMine()
+					g.Board[i+1][j-1].CheckMine()
+					g.Board[i+1][j+1].CheckMine()
+				} else if i == r-1 {
+					g.Board[i-1][j].CheckMine()
+					g.Board[i-1][j-1].CheckMine()
+					g.Board[i-1][j+1].CheckMine()
+					g.Board[i][j-1].CheckMine()
+					g.Board[i][j+1].CheckMine()
+				} else if j == 0 {
+					g.Board[i+1][j].CheckMine()
+					g.Board[i-1][j].CheckMine()
+					g.Board[i+1][j+1].CheckMine()
+					g.Board[i][j+1].CheckMine()
+					g.Board[i-1][j+1].CheckMine()
+				} else if j == c-1 {
+					g.Board[i+1][j].CheckMine()
+					g.Board[i-1][j].CheckMine()
+					g.Board[i+1][j-1].CheckMine()
+					g.Board[i][j-1].CheckMine()
+					g.Board[i-1][j-1].CheckMine()
+				}
+			}
+		}
+	}
+}
+func (cell *Cell) HandleRevealed(g *Game, r, c int) {
+	if cell.IsRevealed || cell.IsMine || cell.IsFlagged {
+		return
+	}
+	cell.IsRevealed = true
+	g.RevealedCells++
+	if cell.NearbyMines == 0 {
+		g.HandleRevealedNeighbours(r, c)
+	}
+}
+func (g *Game) HandleRevealedNeighbours(r, c int) {
+	i := g.Rows
+	j := g.Cols
+	if g.Board[r][c].NearbyMines != 0 {
+		return
+	}
+	if r > 0 && r < i-1 && c > 0 && c < j-1 {
+		g.Board[r-1][c-1].HandleRevealed(g, r-1, c-1)
+		g.Board[r-1][c].HandleRevealed(g, r-1, c)
+		g.Board[r-1][c+1].HandleRevealed(g, r-1, c+1)
+		g.Board[r][c-1].HandleRevealed(g, r, c-1)
+		g.Board[r][c+1].HandleRevealed(g, r, c+1)
+		g.Board[r+1][c-1].HandleRevealed(g, r+1, c-1)
+		g.Board[r+1][c].HandleRevealed(g, r+1, c)
+		g.Board[r+1][c+1].HandleRevealed(g, r+1, c+1)
+	} else if r == 0 && c == 0 {
+		g.Board[0][1].HandleRevealed(g, 0, 1)
+		g.Board[1][0].HandleRevealed(g, 1, 0)
+		g.Board[1][1].HandleRevealed(g, 1, 1)
+	} else if r == 0 && c == j-1 {
+		g.Board[r][c-1].HandleRevealed(g, r, c-1)
+		g.Board[r+1][c].HandleRevealed(g, r+1, c)
+		g.Board[r+1][c-1].HandleRevealed(g, r+1, c-1)
+	} else if r == i-1 && c == 0 {
+		g.Board[r-1][c].HandleRevealed(g, r-1, c)
+		g.Board[r][c+1].HandleRevealed(g, r, c+1)
+		g.Board[r-1][c+1].HandleRevealed(g, r-1, c+1)
+	} else if r == i-1 && c == j-1 {
+		g.Board[r-1][c-1].HandleRevealed(g, r-1, c-1)
+		g.Board[r-1][c].HandleRevealed(g, r-1, c)
+		g.Board[r][c-1].HandleRevealed(g, r, c-1)
+	} else if r == 0 {
+		g.Board[r][c-1].HandleRevealed(g, r, c-1)
+		g.Board[r][c+1].HandleRevealed(g, r, c+1)
+		g.Board[r+1][c].HandleRevealed(g, r+1, c)
+		g.Board[r+1][c-1].HandleRevealed(g, r+1, c-1)
+		g.Board[r+1][c+1].HandleRevealed(g, r+1, c+1)
+	} else if c == 0 {
+		g.Board[r+1][c].HandleRevealed(g, r+1, c)
+		g.Board[r-1][c].HandleRevealed(g, r-1, c)
+		g.Board[r+1][c+1].HandleRevealed(g, r+1, c+1)
+		g.Board[r][c+1].HandleRevealed(g, r, c+1)
+		g.Board[r-1][c+1].HandleRevealed(g, r-1, c+1)
+	} else if r == i-1 {
+		g.Board[r-1][c].HandleRevealed(g, r-1, c)
+		g.Board[r-1][c-1].HandleRevealed(g, r-1, c-1)
+		g.Board[r][c-1].HandleRevealed(g, r, c-1)
+		g.Board[r-1][c+1].HandleRevealed(g, r-1, c+1)
+		g.Board[r][c+1].HandleRevealed(g, r, c+1)
+	} else if c == j-1 {
+		g.Board[r+1][c].HandleRevealed(g, r+1, c)
+		g.Board[r-1][c].HandleRevealed(g, r-1, c)
+		g.Board[r+1][c-1].HandleRevealed(g, r+1, c-1)
+		g.Board[r][c-1].HandleRevealed(g, r, c-1)
+		g.Board[r-1][c-1].HandleRevealed(g, r-1, c-1)
+	}
+}
+func (g *Game) RevealCell(r, c int) {
+	if g.Board[r][c].IsFlagged {
+		os.Stdout.Write([]byte("Flagged cell should not be revealed"))
+		return
+	}
+	if g.Board[r][c].IsMine {
+		g.GameOver = true
+	}
+}
+func (g *Game) FlagCell(r, c int) {
+	if g.Board[r][c].IsRevealed {
+		os.Stdout.Write([]byte("Revealed Cell cannot be flagged"))
+		return
+	}
+
+	g.Board[r][c].IsFlagged = !g.Board[r][c].IsFlagged
+}
+func (g *Game) CheckCompleted() bool {
+	if g.RevealedCells+g.Mines == g.TotalCells {
+		g.GameWon = true
+		return true
+	}
+	return false
+}
+func NewGame(mines, r, c int) *Game {
+	g := &Game{
+		Rows:          r,
+		Cols:          c,
+		GameOver:      false,
+		GameWon:       false,
+		RevealedCells: 0,
+		TotalCells:    r * c,
+		Mines:         mines,
+	}
+	g.Board = make([][]Cell, r)
+	for i := range g.Board {
+		g.Board[i] = make([]Cell, c)
+	}
+	for i := 0; i < r; i++ {
+		for j := 0; j < c; j++ {
+			g.Board[i][j] = Cell{
+				IsRevealed:  false,
+				IsFlagged:   false,
+				NearbyMines: 0,
+			}
+		}
+	}
+	g.PlaceMines()
+	g.CountMines()
+	return g
+}
--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-	"math/rand"
 	"minesweeper/cmd"
+	"minesweeper/internal/engine"
 	"os"
 	"strconv"
 
@@ -13,24 +13,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
-type Cell struct {
-	IsRevealed  bool
-	IsFlagged   bool
-	IsMine      bool
-	NearbyMines int
-}
-type Game struct {
-	Rows          int
-	Cols          int
-	GameOver      bool
-	GameWon       bool
-	Board         [][]Cell
-	TotalCells    int
-	RevealedCells int
-	Mines         int
-}
 type model struct {
-	game     *Game
+	game     *engine.Game
 	progress progress.Model
 	irow     string
 	icol     string
@@ -39,7 +23,7 @@ type model struct {
 
 func initialModel(m, r, c int) model {
 	return model{
-		game:     NewGame(m, r, c),
+		game:     engine.NewGame(m, r, c),
 		isRowSel: true,
 		progress: progress.New(progress.WithDefaultGradient()),
 	}
@@ -65,9 +49,9 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			r, _ := strconv.Atoi(m.irow)
 			c, _ := strconv.Atoi(m.icol)
 			if r >= 0 && r < m.game.Rows && c >= 0 && c < m.game.Cols {
-				m.game.revealCell(r, c)
-				m.game.Board[r][c].handleRevealed(m.game, r, c)
-				m.game.checkCompleted()
+				m.game.RevealCell(r, c)
+				m.game.Board[r][c].HandleRevealed(m.game, r, c)
+				m.game.CheckCompleted()
 			}
 			m.irow = ""
 			m.icol = ""
@@ -76,7 +60,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			r, _ := strconv.Atoi(m.irow)
 			c, _ := strconv.Atoi(m.icol)
 			if r >= 0 && r < m.game.Rows && c >= 0 && c < m.game.Cols {
-				m.game.flagCell(r, c)
+				m.game.FlagCell(r, c)
 			}
 			m.irow = ""
 			m.icol = ""
@@ -185,203 +169,6 @@ func (m model) View() string {
 		s += "\n\033[31mGAME Won! Press 'q' to quit.\033[0m\n"
 	}
 	return s
-}
-
-func (g *Game) placeMines() {
-	mines := g.Mines
-	minesPlaced := 0
-	for minesPlaced < mines {
-		i := rand.Intn(g.Rows)
-		j := rand.Intn(g.Cols)
-		if !g.Board[i][j].IsMine {
-			g.Board[i][j].IsMine = true
-			minesPlaced++
-		}
-	}
-}
-func (cell *Cell) checkMine() {
-	if cell.IsMine {
-	} else {
-		cell.NearbyMines++
-	}
-}
-func (g *Game) countMines() {
-	r := g.Rows
-	c := g.Cols
-	for i := 0; i < r; i++ {
-		for j := 0; j < c; j++ {
-			if g.Board[i][j].IsMine {
-				if i > 0 && i < r-1 && j > 0 && j < c-1 {
-					g.Board[i-1][j-1].checkMine()
-					g.Board[i-1][j].checkMine()
-					g.Board[i-1][j+1].checkMine()
-					g.Board[i][j-1].checkMine()
-					g.Board[i][j+1].checkMine()
-					g.Board[i+1][j-1].checkMine()
-					g.Board[i+1][j].checkMine()
-					g.Board[i+1][j+1].checkMine()
-				} else if i == 0 && j == 0 {
-					g.Board[0][1].checkMine()
-					g.Board[1][0].checkMine()
-					g.Board[1][1].checkMine()
-				} else if i == 0 && j == c-1 {
-					g.Board[i][j-1].checkMine()
-					g.Board[i+1][j].checkMine()
-					g.Board[i+1][j-1].checkMine()
-				} else if i == r-1 && j == 0 {
-					g.Board[i-1][j].checkMine()
-					g.Board[i][j+1].checkMine()
-					g.Board[i-1][j+1].checkMine()
-				} else if i == r-1 && j == c-1 {
-					g.Board[i-1][j-1].checkMine()
-					g.Board[i-1][j].checkMine()
-					g.Board[i][j-1].checkMine()
-				} else if i == 0 {
-					g.Board[i][j-1].checkMine()
-					g.Board[i][j+1].checkMine()
-					g.Board[i+1][j].checkMine()
-					g.Board[i+1][j-1].checkMine()
-					g.Board[i+1][j+1].checkMine()
-				} else if i == r-1 {
-					g.Board[i-1][j].checkMine()
-					g.Board[i-1][j-1].checkMine()
-					g.Board[i-1][j+1].checkMine()
-					g.Board[i][j-1].checkMine()
-					g.Board[i][j+1].checkMine()
-				} else if j == 0 {
-					g.Board[i+1][j].checkMine()
-					g.Board[i-1][j].checkMine()
-					g.Board[i+1][j+1].checkMine()
-					g.Board[i][j+1].checkMine()
-					g.Board[i-1][j+1].checkMine()
-				} else if j == c-1 {
-					g.Board[i+1][j].checkMine()
-					g.Board[i-1][j].checkMine()
-					g.Board[i+1][j-1].checkMine()
-					g.Board[i][j-1].checkMine()
-					g.Board[i-1][j-1].checkMine()
-				}
-			}
-		}
-	}
-}
-func (cell *Cell) handleRevealed(g *Game, r, c int) {
-	if cell.IsRevealed || cell.IsMine || cell.IsFlagged {
-		return
-	}
-	cell.IsRevealed = true
-	g.RevealedCells++
-	if cell.NearbyMines == 0 {
-		g.handleRevealedNeighbours(r, c)
-	}
-}
-func (g *Game) handleRevealedNeighbours(r, c int) {
-	i := g.Rows
-	j := g.Cols
-	if g.Board[r][c].NearbyMines != 0 {
-		return
-	}
-	if r > 0 && r < i-1 && c > 0 && c < j-1 {
-		g.Board[r-1][c-1].handleRevealed(g, r-1, c-1)
-		g.Board[r-1][c].handleRevealed(g, r-1, c)
-		g.Board[r-1][c+1].handleRevealed(g, r-1, c+1)
-		g.Board[r][c-1].handleRevealed(g, r, c-1)
-		g.Board[r][c+1].handleRevealed(g, r, c+1)
-		g.Board[r+1][c-1].handleRevealed(g, r+1, c-1)
-		g.Board[r+1][c].handleRevealed(g, r+1, c)
-		g.Board[r+1][c+1].handleRevealed(g, r+1, c+1)
-	} else if r == 0 && c == 0 {
-		g.Board[0][1].handleRevealed(g, 0, 1)
-		g.Board[1][0].handleRevealed(g, 1, 0)
-		g.Board[1][1].handleRevealed(g, 1, 1)
-	} else if r == 0 && c == j-1 {
-		g.Board[r][c-1].handleRevealed(g, r, c-1)
-		g.Board[r+1][c].handleRevealed(g, r+1, c)
-		g.Board[r+1][c-1].handleRevealed(g, r+1, c-1)
-	} else if r == i-1 && c == 0 {
-		g.Board[r-1][c].handleRevealed(g, r-1, c)
-		g.Board[r][c+1].handleRevealed(g, r, c+1)
-		g.Board[r-1][c+1].handleRevealed(g, r-1, c+1)
-	} else if r == i-1 && c == j-1 {
-		g.Board[r-1][c-1].handleRevealed(g, r-1, c-1)
-		g.Board[r-1][c].handleRevealed(g, r-1, c)
-		g.Board[r][c-1].handleRevealed(g, r, c-1)
-	} else if r == 0 {
-		g.Board[r][c-1].handleRevealed(g, r, c-1)
-		g.Board[r][c+1].handleRevealed(g, r, c+1)
-		g.Board[r+1][c].handleRevealed(g, r+1, c)
-		g.Board[r+1][c-1].handleRevealed(g, r+1, c-1)
-		g.Board[r+1][c+1].handleRevealed(g, r+1, c+1)
-	} else if c == 0 {
-		g.Board[r+1][c].handleRevealed(g, r+1, c)
-		g.Board[r-1][c].handleRevealed(g, r-1, c)
-		g.Board[r+1][c+1].handleRevealed(g, r+1, c+1)
-		g.Board[r][c+1].handleRevealed(g, r, c+1)
-		g.Board[r-1][c+1].handleRevealed(g, r-1, c+1)
-	} else if r == i-1 {
-		g.Board[r-1][c].handleRevealed(g, r-1, c)
-		g.Board[r-1][c-1].handleRevealed(g, r-1, c-1)
-		g.Board[r][c-1].handleRevealed(g, r, c-1)
-		g.Board[r-1][c+1].handleRevealed(g, r-1, c+1)
-		g.Board[r][c+1].handleRevealed(g, r, c+1)
-	} else if c == j-1 {
-		g.Board[r+1][c].handleRevealed(g, r+1, c)
-		g.Board[r-1][c].handleRevealed(g, r-1, c)
-		g.Board[r+1][c-1].handleRevealed(g, r+1, c-1)
-		g.Board[r][c-1].handleRevealed(g, r, c-1)
-		g.Board[r-1][c-1].handleRevealed(g, r-1, c-1)
-	}
-}
-func (g *Game) revealCell(r, c int) {
-	if g.Board[r][c].IsFlagged {
-		os.Stdout.Write([]byte("Flagged cell should not be revealed"))
-		return
-	}
-	if g.Board[r][c].IsMine {
-		g.GameOver = true
-	}
-}
-func (g *Game) flagCell(r, c int) {
-	if g.Board[r][c].IsRevealed {
-		os.Stdout.Write([]byte("Revealed Cell cannot be flagged"))
-		return
-	}
-
-	g.Board[r][c].IsFlagged = !g.Board[r][c].IsFlagged
-}
-func (g *Game) checkCompleted() bool {
-	if g.RevealedCells+g.Mines == g.TotalCells {
-		g.GameWon = true
-		return true
-	}
-	return false
-}
-func NewGame(mines, r, c int) *Game {
-	g := &Game{
-		Rows:          r,
-		Cols:          c,
-		GameOver:      false,
-		GameWon:       false,
-		RevealedCells: 0,
-		TotalCells:    r * c,
-		Mines:         mines,
-	}
-	g.Board = make([][]Cell, r)
-	for i := range g.Board {
-		g.Board[i] = make([]Cell, c)
-	}
-	for i := 0; i < r; i++ {
-		for j := 0; j < c; j++ {
-			g.Board[i][j] = Cell{
-				IsRevealed:  false,
-				IsFlagged:   false,
-				NearbyMines: 0,
-			}
-		}
-	}
-	g.placeMines()
-	g.countMines()
-	return g
 }
 func main() {
 	cmd.RootCmd.Run = func(cd *cobra.Command, args []string) {
